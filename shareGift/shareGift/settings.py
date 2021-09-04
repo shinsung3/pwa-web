a01a60ab962f1259de0b373c7af4843afbb4b3db
nustar <tlstjd436@naver.com>
2021-09-05 00:12:58 +0900

    #210905 - IP 테스트

--- shareGift/shareGift/settings.py
+++ shareGift/shareGift/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g@i5r*)-smm5u*g9-nebt^6mltovbb!afhhvj!3uhqa^*!e^)d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '3.36.54.241']
+ALLOWED_HOSTS = ['127.0.0.1', '3.36.54.241', '3.38.92.156']
 
 # Application definition
 
